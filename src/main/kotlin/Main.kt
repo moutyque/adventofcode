5949e7fc171ac8c770154bf748da5020fb2f5d47
Quentin MOUTY <quentin.mouty@corp.ovh.com>
2022-12-05 08:14:36 +0100

    Day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,52 @@ fun main(args: Array<String>) {
 
 }
 
+fun day11() {
+    //https://adventofcode.com/2022/day/1
+    var first = 0
+    var currentValue = 0
+    var line = ""
+    do {
+        line = readln()
+        with(line) {
+            when (this) {
+                "" -> {
+                    first = maxOf(first, currentValue)
+                    currentValue = 0
+                }
+
+                else -> {
+                    currentValue += this.toInt()
+                }
+            }
+        }
+    } while (line != "\n")
+
+}
+
+fun day12() {
+    //https://adventofcode.com/2022/day/1
+    var currentValue = 0
+    var line = ""
+    val elves = mutableListOf<Int>()
+    do {
+        line = readln()
+        with(line) {
+            when (this.isEmpty()) {
+                true -> {
+                    elves.add(currentValue)
+                    elves.sortDescending()
+                    if (elves.size > 3) println(elves[0] + elves[1] + elves[2])
+                    currentValue = 0
+                }
+
+                false -> {
+                    currentValue += this.toInt()
+                }
+            }
+        }
+    } while (line != "\n")
+
+}
 
 

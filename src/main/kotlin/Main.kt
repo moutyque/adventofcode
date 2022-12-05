0792be1957794f1eaa9e8d023c9033aa2434f1e4
Quentin MOUTY <quentin.mouty@corp.ovh.com>
2022-12-05 08:15:49 +0100

    Day 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -190,3 +190,47 @@ fun day31() {
     }
 }
 
+fun day41() {
+    var line = readln()
+    var score = 0
+    while (line.isNotBlank()) {
+
+        val (r1, r2) = line.split(",").run {
+            Pair(this.first().split("-").run {
+                (this.first().toInt()..this.last().toInt()).toList()
+            }, this.last().split("-").run {
+                (this.first().toInt()..this.last().toInt()).toList()
+            })
+        }
+
+        if (r1.containsAll(r2)  || r2.containsAll(r1)) {
+            score += 1
+        }
+        line = readln()
+    }
+    println(score)
+}
+
+fun day42() {
+    var line = readln()
+    var score = 0
+    while (line.isNotBlank()) {
+
+        val (r1, r2) = line.split(",").run {
+            Pair(this.first().split("-").run {
+                (this.first().toInt()..this.last().toInt())
+            }, this.last().split("-").run {
+                (this.first().toInt()..this.last().toInt())
+            })
+        }
+
+        for (i in r1) {
+            if (i in r2) {
+                score += 1
+                break
+            }
+        }
+        line = readln()
+    }
+    println(score)
+}

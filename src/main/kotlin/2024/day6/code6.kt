be0df70264a509462644a3fe323ab2c595af9a00
Quentin MOUTY <quentin.mouty@ovhcloud.com>
2024-12-09 09:00:56 +0100

    2024 - Day 6
    (cherry picked from commit 07828b8c29d84d27d7ee69858c3993f0c97bf229)

--- src/main/kotlin/2024/day6/code6.kt
+++ src/main/kotlin/2024/day6/code6.kt
@@ -14,9 +14,9 @@ fun main() {
     require(actual == 41)
     File("${prefix}/1").prepare().compute61().let { println(it) }
     val actual2 = File("${prefix}/test").readLines().asSequence().filter { it.isNotBlank() }.compute62()
-        println(actual2)
-        require(actual2 == 6)
-    // File("${prefix}/1").readLines().asSequence().compute62().let { println(it) }
+    println(actual2)
+    require(actual2 == 6)
+    File("${prefix}/1").readLines().asSequence().compute62().let { println(it) }
 }
 
 fun File.prepare() = this.readLines().asSequence().filter { it.isNotBlank() }
@@ -28,61 +28,80 @@ val left = Pair(0, -1)
 val direction = listOf(up, right, down, left)
 var currentMove = up
 var position = Pair(0, 0)
-var moveIndex = 0
 fun Sequence<String>.compute61(): Int {
     val grid = this.map { it.split("") }.toList()
     grid.findStartPosition()
     val crossedPosition = mutableSetOf<Pair<Int, Int>>()
-    moveIndex = direction.indexOf(currentMove)
+    var moveIndex = direction.indexOf(currentMove)
     //Until out apply move
     while (position.first in grid.indices && position.second in grid.first().indices) {
-        try{
+        try {
             if (grid.get(position.plus(currentMove)) == "#") {
                 moveIndex = (moveIndex + 1) % direction.size
                 currentMove = direction[moveIndex]
             }
             crossedPosition.add(position)
             position = position.plus(currentMove)
-        } catch (e: java.lang.IndexOutOfBoundsException){
+        } catch (e: java.lang.IndexOutOfBoundsException) {
             position = position.plus(currentMove)
         }
     }
-    return crossedPosition.size +1
+    return crossedPosition.size + 1
 }
 
 //Change to find if any other square using next position on the same line until end of grid
 fun Sequence<String>.compute62(): Int {
-    var possibleBlock = 0
     val grid = this.map { it.split("") }.toList()
     grid.findStartPosition()
     val crossedPosition = mutableSetOf<Pair<Int, Int>>()
     crossedPosition.add(position)
-    moveIndex = direction.indexOf(currentMove)
+    var moveIndex = direction.indexOf(currentMove)
+    var candidatesCount = 0
     //Until out apply move
     while (position.first in grid.indices && position.second in grid.first().indices) {
-        try{
+        try {
             if (grid.get(position.plus(currentMove)) == "#") {
-                moveIndex = (moveIndex + 1) % direction.size
+                moveIndex = moveIndex.nextPositionIndex()
                 currentMove = direction[moveIndex]
+            } else {
+                crossedPosition.add(position)
+                if(isPattern(grid, direction[moveIndex.nextPositionIndex()], position, crossedPosition)){
+                    candidatesCount++
+                }
             }
-
-            if(!crossedPosition.add(position)){
-                possibleBlock++
-            }
-
             position = position.plus(currentMove)
-        } catch (e: java.lang.IndexOutOfBoundsException){
+        } catch (e: java.lang.IndexOutOfBoundsException) {
             position = position.plus(currentMove)
         }
     }
-    return possibleBlock
+    return candidatesCount
 }
 
-fun List<List<String>>.isClose(position: Pair<Int, Int>) : Boolean=
-    direction.any {
-        this.get(position.plus(up)) == "#"
+fun isPattern(
+    grid: List<List<String>>,
+    direction: Pair<Int, Int>,
+    initPosition: Pair<Int, Int>,
+    crossedPosition: Set<Pair<Int, Int>>
+): Boolean {
+    var position = initPosition
+    var response = false
+    var count = 0
+    try {
+        while (grid.get(position) != "#") {
+            position = position.plus(direction)
+            if(crossedPosition.contains(position)){
+                count++
+            }
+        }
+        if (count > 0) response = true
+    } catch (e: java.lang.IndexOutOfBoundsException) {
+        response = false
     }
 
+    return response
+}
+
+fun Int.nextPositionIndex() = (this + 1) % direction.size
 
 
 fun List<List<String>>.get(position: Pair<Int, Int>) = this[position.first][position.second]

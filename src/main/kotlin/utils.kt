d24daef49a9c1745787e2d4fb076c3eced639e9b
Quentin MOUTY <quentin.mouty@ovhcloud.com>
2024-12-18 08:30:42 +0100

    Refactor

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -6,3 +6,17 @@ typealias Line = String
 fun Sequence<String>.buildGrid(): List<List<String>> = this.map { line ->
     line.split("").filter { it.isNotBlank() }.toList()
 }.toList()
+
+fun Sequence<String>.buildIntGrid(): MutableList<MutableList<Int>> = this.map { line ->
+    line.split("").filter { it.isNotBlank() }.map { it.toInt() }.toMutableList()
+}.toMutableList()
+
+
+val up = Pair(-1, 0)
+val down = Pair(1, 0)
+val right = Pair(0, 1)
+val left = Pair(0, -1)
+val directions = listOf(up, right, down, left)
+
+fun <T: Any> List<List<T>>.getValue(position: Pair<Int, Int>)= this[position.first][position.second]
+

5c73b7f86bd64ee29ab006a6a194e24ae5c94488
Quentin MOUTY <quentin.mouty@ovhcloud.com>
2024-12-20 09:01:42 +0100

    2024 - Day 11
    (cherry picked from commit 5c0ecfc81f7533bc3b8cdd2953a5900bab90c7e1)

--- src/main/kotlin/2024/day11/code11.kt
+++ src/main/kotlin/2024/day11/code11.kt
@@ -8,46 +8,39 @@ const val day = 11
 val prefix = "./src/main/kotlin/2024/day$day"
 
 fun main() {
-    File("$prefix/test").prepare().compute111().also { println(it) }.also { require(it == 55312) }
-    File("${prefix}/input").prepare().compute111().let { println(it) }
+    File("$prefix/test").prepare().compute11().also { println(it) }.also { require(it == 55312L) }
+    cache.clear()
+    File("${prefix}/input").prepare().compute11().let { println(it) }
+    cache.clear()
+    File("${prefix}/input").prepare().compute11(75).let { println(it) }
 
 }
 
-fun Sequence<String>.compute111(): Int {
-    val initialStones = this.flatMap {
-        it.split(" ").filter { it.isNotBlank() }.map { it.toLong() }.map { Stone(it) }
-    }.toMutableList()
-    val updateStones = mutableListOf<Stone>()
-    for (i in 1..25) {
-        for (s in initialStones) {
-            updateStones.addAll(s.blink())
+fun Sequence<String>.compute11(rounds: Int = 25): Long {
+    val stones = this.flatMap {
+        it.split(" ").filter { it.isNotBlank() }.map { it.toLong() }.map { it }
+    }
+    return stones.sumOf { it.blink(rounds) }
+}
+
+val cache = mutableMapOf<Pair<Int, Long>, Long>()
+
+fun Long.blink(maxRound: Int = 25, currentRound: Int = 0): Long =
+    cache.getOrPut(Pair(currentRound, this)) {
+        when {
+            currentRound == maxRound -> 1
+            this == 0L -> 1L.blink(maxRound, currentRound + 1)
+            toString().length % 2 == 0 -> this.split().sumOf { it.blink(maxRound, currentRound + 1) }
+            else -> (this * 2024).blink(maxRound, currentRound + 1)
         }
-        initialStones.clear()
-        initialStones.addAll(updateStones)
-        updateStones.clear()
     }
 
-    return initialStones.size
-}
 
-data class Stone(val value: Long) {
-    fun blink() =
-        when {
-            this.value == 0L -> listOf(Stone(1))
-            this.value.toString().length % 2 == 0 -> this.split()
-            else -> listOf(Stone(this.value * 2024))
-        }
+private fun Long.split(): List<Long> = listOf(
+    this.subLong(0, this.toString().length / 2),
+    this.subLong(this.toString().length / 2, this.toString().length)
 
-
-    fun split(): List<Stone> = listOf(
-        Stone(
-            this.value.subLong(0, this.value.toString().length / 2)
-        ),
-        Stone(
-            this.value.subLong(this.value.toString().length / 2, this.value.toString().length)
-        )
-    )
-}
+)
 
 fun Long.subLong(from: Int = 0, to: Int = 0): Long =
     this.toString().toCharArray().toList().subList(from, to).joinToString("").toLong()
